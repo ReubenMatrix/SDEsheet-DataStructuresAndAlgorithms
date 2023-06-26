68693bab410e5b8346cf14913cd461d020d81071
ReubenMatrix <136352370+ReubenMatrix@users.noreply.github.com>
2023-06-26 11:58:27 +0530

    Add files via upload

---
+++ BinarySearch/Arrays2D/BinarySearchIn2D.java
@@ -0,0 +1,30 @@
+package BinarySearch.Arrays2D;
+
+public class BinarySearchIn2D {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean searchMatrix(int[][] matrix, int target) {
+            int low = 0;
+            if(matrix.length == 0) return false;
+            int n = matrix.length;
+            int m = matrix[0].length;
+            int high = (n * m) - 1;
+
+            while(low <= high) {
+                int mid = (low + (high - low) / 2);
+                if(matrix[mid/m][mid % m] == target) {
+                    return true;
+                }
+                if(matrix[mid/m][mid % m] < target) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+            return false;
+    }
+}
+
