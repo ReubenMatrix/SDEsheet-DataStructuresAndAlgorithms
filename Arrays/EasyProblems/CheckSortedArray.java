b0387373e0232d73ee7c006e7e35493c09bb504b
ReubenMatrix <136352370+ReubenMatrix@users.noreply.github.com>
2023-06-29 09:51:35 +0530

    Update CheckSortedArray.java

--- Arrays/EasyProblems/CheckSortedArray.java
+++ Arrays/EasyProblems/CheckSortedArray.java
@@ -1,5 +1,14 @@
 package Arrays.EasyProblems;
 
+/*
+Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not.
+If the array is sorted then return True, Else return False.
+
+Example :
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: True.
+*/
+
 public class CheckSortedArray {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5};
