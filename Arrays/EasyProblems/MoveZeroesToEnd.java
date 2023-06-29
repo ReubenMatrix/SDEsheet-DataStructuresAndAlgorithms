43071f56171caeca9a4cd87334b783b43b2d03ba
ReubenMatrix <136352370+ReubenMatrix@users.noreply.github.com>
2023-06-29 10:02:43 +0530

    Update MoveZeroesToEnd.java

--- Arrays/EasyProblems/MoveZeroesToEnd.java
+++ Arrays/EasyProblems/MoveZeroesToEnd.java
@@ -1,5 +1,12 @@
 package Arrays.EasyProblems;
 
+/*
+You are given an array of integers, your task is to move all the zeros in the array to the end of the array 
+and move non-negative integers to the front by maintaining their order.
+
+Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
+Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
+*/
 public class MoveZeroesToEnd {
     public static void main(String[] args) {
         int[] arr={0, 1, 4, 0, 8, 7, 0, 2};
